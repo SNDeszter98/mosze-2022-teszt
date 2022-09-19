f8cde9feddd34cd15e0ba498e72f947cf765f4c7
SNDeszter98 <saandor.eszter98@gmail.com>
2022-09-19 18:30:57 +0200

    hibak kommentezve

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //hianyzo alavonas "_"
+    std::cout << '1-100 ertekek duplazasa' // ' helyett " es ; hiany
+    for (int i = 0;) //lepeskoz es feltetel (i<N_ELEMENTS) hianyzik
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //feltetel (i<N_ELEMENTS) hianyzik
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek:" // ertekkiiras es sorzaras hianyzik
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //hianyzo ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
